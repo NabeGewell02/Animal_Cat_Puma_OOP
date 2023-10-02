8cfd998b9fef376e07fedd1f98f136eb455be99b
student_ait409 <talyshevmihail06@gmail.com>
2023-10-02 10:40:22 +0300

    privedenie classov

--- app/src/main/java/com/example/animal_cat_puma/MainActivity.java
+++ app/src/main/java/com/example/animal_cat_puma/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.animal_cat_puma;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -25,5 +28,27 @@ public class MainActivity extends AppCompatActivity {
 
         Cat murzik = new Lion(3,"Mufasa");
         murzik.talk();
+
+        ArrayList<Cat> cats = new ArrayList<>();
+        cats.add(vaska);
+        cats.add(puma);
+        cats.add(puma1);
+        cats.add(murzik);
+        Cat cat = cats.get(0);
+        Cat cat1 = cats.get(1);
+        Cat cat2 = cats.get(2);
+        Cat cat3 = cats.get(3);
+
+        Puma puma1_1 = (Puma) cats.get(1);
+        puma1.talk();
+
+        Cat cat5 = cats.get(3);
+        if (cat5 instanceof Puma) {
+            Puma puma3 = (Puma) cat5;
+            puma3.talk();
+        } else {
+            Log.i("instanceof", "This is not Puma");
+
+        }
     }
 }
